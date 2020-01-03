7f2ed76db2bdfcc6cbd7a4ff64856701361be376
Dwi Marfia <34159879+dwimarfia@users.noreply.github.com>
2020-01-03 09:25:59 +0700

    Create histats.js

---
+++ histats.js
@@ -0,0 +1,9 @@
+var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,3863678,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
